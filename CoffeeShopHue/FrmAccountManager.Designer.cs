3483aa6488e00ded1744509d056ce9ecc6f24bbf
Tri Phan <azuetest@gmail.com>
2018-04-11 15:13:15 +0700

    [New] Account Manager
    Add new Account Manager form

---
+++ CoffeeShopHue/FrmAccountManager.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CoffeeShopHue
+{
+    partial class FrmAccountManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmAccountManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 402);
+            this.Name = "FrmAccountManager";
+            this.Text = "FrmAccountManager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
